088f3c4a2ddb3824e8cfcd83129187221c24c39d
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2022-10-06 18:35:54 +0530

    added timer screen

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const dobPickerForm = document.getElementById("dob-picker");
 const dobInput = document.getElementById("dob");
-let userDob;
+const timerContainer = document.getElementById("timer-container");
 
 dobPickerForm.addEventListener("submit", (e) => {
 	e.preventDefault();
-	userDob = dob.value;
+	let userDob = dob.value;
 	dobPickerForm.classList.add("remove-element");
-	setTimeout(() => (dobPickerForm.style.display = "none"), 600);
+	setTimeout(() => {
+		dobPickerForm.style.display = "none";
+		timerContainer.classList.add("add-element");
+		timerContainer.style.display = "flex";
+		document.body.style.alignItems = "start";
+	}, 500);
+	displayTimer(userDob);
+	displayLifeInWeeks(userDob);
 });
+
+const displayTimer = (dobInString) => {
+	console.log(dobInString);
+};
+const displayLifeInWeeks = (dobInString) => {};
