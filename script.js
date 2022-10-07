b792399edfbedf145c324ef08955c585f0b6f82f
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2022-10-07 20:02:05 +0530

    changed website style and made it responsive

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
 const dobPickerForm = document.getElementById("dob-picker");
 const dobInput = document.getElementById("dob");
 const timerContainer = document.getElementById("timer-container");
-const timer = document.getElementById("timer");
+const daysToDisplay = document.querySelector("#clock-container #days h1");
+const timeToDisplay = document.querySelector("#clock-container #hours h1");
 const pauseBtn = document.getElementById("pause-btn");
+const lifeInWeeksContainer = document.getElementById("life-in-week-container");
 
+var userDob = null;
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
 let days = 0;
 
+(function () {
+	if (localStorage.getItem("userDob")) {
+		userDob = new Date(Date.parse(localStorage.getItem("userDob")));
+		console.log("Saved DOB fetched", userDob);
+		dobPickerForm.style.display = "none";
+		timerContainer.style.display = "flex";
+		document.body.style.display = "block";
+		displayTimer(userDob);
+		displayLifeInWeeks();
+	}
+})();
+
 dobPickerForm.addEventListener("submit", (submit) => {
 	submit.preventDefault();
-	let userDob = dob.valueAsDate;
+	userDob = dob.valueAsDate;
 	if (!userDob) return;
+
+	localStorage.setItem("userDob", userDob.toString());
+
 	dobPickerForm.classList.add("remove-element");
 	setTimeout(() => {
 		dobPickerForm.style.display = "none";
 		timerContainer.classList.add("add-element");
 		timerContainer.style.display = "flex";
-		document.body.style.alignItems = "start";
+		document.body.style.display = "block";
 	}, 500);
 	displayTimer(userDob);
-	displayLifeInWeeks(userDob);
+	displayLifeInWeeks();
 });
 
-const displayTimer = (dobAsNumber) => {
+function displayTimer(dobAsNumber) {
 	let today = new Date();
 	let _80yearsFromNow = new Date(dobAsNumber.getTime() + 2524556160000); //adding 80 years to dob
 
@@ -36,36 +54,43 @@ const displayTimer = (dobAsNumber) => {
 	seconds = 59;
 
 	const timerInterval = setInterval(() => {
-		timer.innerHTML = getRemainingTime();
-		if (hours === 0 && minutes === 0 && seconds === 0) {
+		let timeLeft = getRemainingTime();
+		// console.log(timeLeft);
+		// let element = document.createElement("div");
+		daysToDisplay.innerHTML = timeLeft[0];
+		// daysToDisplay.append(element);
+		timeToDisplay.innerHTML = timeLeft[1];
+		// element.innerHTML = timeLeft[1];
+		// timeToDisplay.append(element);
+
+		if (days === 0 && hours === 0 && minutes === 0 && seconds === 0) {
 			clearInterval(timerInterval);
 		}
 	}, 1000);
-};
+}
 
 function getRemainingTime() {
 	seconds--;
-	if (seconds === 0) {
-		seconds = 60;
+	if (seconds === -1) {
+		seconds = 59;
 		minutes--;
 	}
-	if (minutes === 0) {
-		minutes = 60;
+	if (minutes === -1) {
+		minutes = 59;
 		hours--;
 	}
 
 	if (hours === 0) {
-		hours = 24;
+		hours = 23;
 		days--;
 	}
 
-	return `${days} Days<span class="important">,</span> ${
-		hours > 9 ? hours : "0" + hours
-	}<span class="important">:</span>${
-		minutes > 9 ? minutes : "0" + minutes
-	}<span class="important">:</span>${
-		seconds > 9 ? seconds : "0" + seconds
-	} <span style="text-decoration: underline;">left</span>`;
+	return [
+		`${days}`,
+		`${hours > 9 ? hours : "0" + hours}<span class="important">:</span>${
+			minutes > 9 ? minutes : "0" + minutes
+		}<span class="important">:</span>${seconds > 9 ? seconds : "0" + seconds}`,
+	];
 }
 
 pauseBtn.addEventListener(
@@ -73,4 +98,13 @@ pauseBtn.addEventListener(
 	() => (pauseBtn.innerText = "Sadly it doesn't work that way ☠️")
 );
 
-const displayLifeInWeeks = (dobInString) => {};
+function displayLifeInWeeks() {
+	let weeksLeft = days / 7;
+	for (i = 0; i < 4170; i++) {
+		let weekBox = document.createElement("div");
+		i <= 4170 - weeksLeft
+			? weekBox.classList.add("box-fill")
+			: weekBox.classList.add("box");
+		lifeInWeeksContainer.append(weekBox);
+	}
+}
